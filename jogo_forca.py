729274be275d6c88541ad51c5168036216479bec
Isla Artemes <islaartemes@gmail.com>
2022-05-23 21:47:26 -0300

    primeiro commit

---
+++ jogo_forca.py
@@ -0,0 +1,31 @@
+from random import randint
+
+menu = """
+>>>>>>>>>>>>     UNIESP     <<<<<<<<<<<<
+#       INTRODUÇÃO A PROGRAMAÇÃO       #
+#                                      #
+#              CATEGORIA               #     
+#       1- FRUTA                       #
+#       2- ANIMAL                      #
+#       3- PAÍS                        #
+#       DIGITE OUTRA TECLA PARA SAIR   #
+#                                      #
+#                                      #
+#   GRUPO:                             #
+#   GABRIELA -                         #
+#   GUSTAVO -                          #
+#   ISLA -                             #
+#   LUAN -                             #
+#   Mª ALICE -                         #
+########################################
+
+"""
+
+opcao_jogo = "1"
+
+categorias = [
+    ["abacaxi", "abacate", "amora", "banana", "bacuri", "buriti", "caju", "carambola", "cacau", "damasco", "figo", "framboesa", "goiaba", "graviola", "jambo", "jabuticaba", "kiwi", "laranja", "nectarina", "seriguela"],
+    ["abelha","abutre","albatroz","alce","alpaca","anaconda","anchova","andorinha","anta","aranha","arara","asno","atum","avestruz","bacalhau","bacuri","badejo","bagre","baiacu","baleia","barata","barbo","cabra","cachalote","cachorro","camelo","camundongo","canguru","dinossauro","doninha","dourado","elefante","gafanhoto","gaivota","galinha","hadoque","hamster","iguana","iguanara","impala","jabiru","jabuti"],
+    ["Alemanha","Andorra","Angola","Argentina","Barbados","Belize","Benim","Bielorrússia","Cabo Verde","Camarões","Camboja","Chile","China","Dinamarca","Egito","Equador","Filipinas","Guiana","Holanda","Iraque","Irlanda","Jamaica","Portugal","Turquia","Uruguai"]
+]
+
